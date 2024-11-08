0f23bb5903d9fcd8cc71102cc1434e3c2f962cf8
philippocalippo <philhofmann@outlook.com>
2024-11-08 11:23:16 +0100

    Turned to Single Source of Truth

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
 if __name__ == "__main__":
     setup(
-        name='lpfun',
-        author='Phil-Alexander Hofmann',
-        author_email='mail@philhofmann.de',
-        description="A package for fast polynomial interpolation and differentiation.", 
-        python_requires='>=3.8',
-        version='0.0.1',
-        packages=find_packages(where='src'),
-        package_dir={'': 'src'},
-        install_requires=[
-            'numpy==1.22.4',
-            'numba==0.58.1',
-            'pytest==8.2.2',
-            'setuptools==70.1.1'
-        ],
+        name="lpfun",
+        author="Phil-Alexander Hofmann",
+        author_email="mail@philhofmann.de",
+        description="A package for fast polynomial interpolation and differentiation.",
+        python_requires=">=3.8",
+        version="0.0.1",
+        packages=find_packages(where="src"),
+        package_dir={"": "src"},
+        install_requires=install_requires,
     )
